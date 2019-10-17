18345fce3dbd8083a0f82cbbe43591b540f81e43
bhavyameghnani <meghnani.bhavya@gmail.com>
2019-10-17 18:04:42 +0800

    Second commit

--- app/src/main/java/com/example/daimlerhackasia/MainActivity.java
+++ app/src/main/java/com/example/daimlerhackasia/MainActivity.java
@@ -2,13 +2,76 @@ package com.example.daimlerhackasia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    public ImageView btnLogOut;
+    ImageView i1,i2,i3,i4,i5,i6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide(); // hide the title bar
+
+        i1 = findViewById(R.id.markZone);
+        i2 = findViewById(R.id.queueMgt);
+        i3 = findViewById(R.id.autogen);
+        i4 = findViewById(R.id.rewards);
+        i5 = findViewById(R.id.Map);
+        i6 = findViewById(R.id.SOS);
+        btnLogOut = findViewById(R.id.btnLogOut);
+
+        i1.setOnClickListener(this);
+        i2.setOnClickListener(this);
+        i3.setOnClickListener(this);
+        i4.setOnClickListener(this);
+        i5.setOnClickListener(this);
+        i6.setOnClickListener(this);
+
+
+        btnLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                FirebaseAuth.getInstance().signOut();
+                Intent I = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(I);
+            }
+        });
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        /*if(view == i1){
+            startActivity(new Intent(this, markZoneActivity.class));
+        }
+        else if(view == i2){
+            startActivity(new Intent(this, qMgmtActivity.class));
+        }
+        else if(view == i3){
+            startActivity(new Intent(this, AutoGenActivity.class));
+        }
+        else if(view == i4){
+            startActivity(new Intent(this, RewardsActivity.class));
+        }
+        else if(view == i5){
+            startActivity(new Intent(this, MapsActivity.class));
+        }
+        else if(view == i6){
+            startActivity(new Intent(this, SOSActivity.class));
+        }*/
+
     }
 }
---
+++ app/src/main/java/com/example/daimlerhackasia/SplashScreenActivity.java
@@ -0,0 +1,29 @@
+package com.example.daimlerhackasia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 1500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        getSupportActionBar().hide(); // hide the title bar
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreenActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
